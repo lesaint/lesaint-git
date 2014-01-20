70a4e15f76d0739e35ac34fdb30600c71ec87f52
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2014-01-20 23:39:31 +0100

    initial commit of lesaint-git.plugin.zsh

---
+++ lesaint-git.plugin.zsh
@@ -0,0 +1,16 @@
+# I prefer using gci instead of gc
+unalias gc
+alias gci="git commit -v"
+compdef _git gci=git-commit
+
+# Performs a merge from a branch into another, making sure local checkout is up-to-date in both branches
+# 
+# $1 : merge source
+# $2 : merge target
+function gitMergeAfterPull() {
+  gco $1
+  gup
+  gco $2
+  gup
+  g merge $1
+}
