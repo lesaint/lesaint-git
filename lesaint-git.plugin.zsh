c4cb4e5d0dce2a82e49d6672223551aeaaa00881
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2016-04-28 14:01:35 +0200

    add gcif alias

--- lesaint-git.plugin.zsh
+++ lesaint-git.plugin.zsh
@@ -1,6 +1,7 @@
 # I prefer using gci instead of gc
 unalias gc
 alias gci="git commit -v"
+alias gcif="git commit -v --fixup"
 compdef _git gci=git-commit
 
 # Performs a merge from a branch into another, making sure local checkout is up-to-date in both branches
